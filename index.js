8d928271e770dc64e508cef6f09810033d029c09
katwits <160411607+katwits@users.noreply.github.com>
2024-07-01 13:46:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,14 @@ document.addEventListener('DOMContentLoaded', () => {
         const tiltY = event.beta;  // Front-to-back tilt in degrees
 
         // Adjust the sensitivity if needed
-        const sensitivity = 0.81;
+        const sensitivity = 0.5;
 
         // Update ball position based on tilt
         ballX += tiltX * sensitivity;
         ballY += tiltY * sensitivity;
+
+        ballX = Math.max(0, Math.min(gameArea.clientWidth - ball.clientWidth, ballX));
+        ballY = Math.max(0, Math.min(gameArea.clientHeight - ball.clientHeight, ballY));
 
         updateBallPosition();
     }
